bf7561e0ef00f34f5ae2afce8881bb85bd0d1232
Tony Huang <antiyourself@hotmail.com>
2020-03-22 18:34:12 +0800

    third edition

--- password.py
+++ password.py
@@ -1,12 +1,16 @@
 chance = 3
 my_password = 'a123456'
 while chance > 0:
+	chance = chance - 1
 	password = input('輸入密碼: ')
 	if password == my_password:
 		print('登入成功')
 		break
 
 	else:
-		chance = chance - 1
-		print('密碼錯誤! 還有 ', chance, '次機會')
+		print('密碼錯誤!')
+		if chance > 0:
+			print('還有', chance, '次機會')
+		else:
+			print('已無機會')
 		
